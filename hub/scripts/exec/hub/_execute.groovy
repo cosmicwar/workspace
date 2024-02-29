11674b86f9f95306a769e3dac267f53a767e5ae0
ily2bb <58750338+ily2bb@users.noreply.github.com>
2024-02-29 17:56:17 -0500

    re-setup modules / starcade

--- hub/scripts/exec/hub/_execute.groovy
+++ hub/scripts/exec/hub/_execute.groovy
@@ -15,20 +15,21 @@ setJar("entropy-bundler-1.20.1-R0.9-reobf.jar")
 if (Temple.templeEnv == "dev") addArgs("-DdevMode=true")
 
 execute {
-    loadFromConfig("configs/global/bukkit.yml", "bukkit.yml")
-    if (Temple.templeEnv != "dev") {
-        loadFromConfig("configs/global/spigot.yml", "spigot.yml")
-        loadFromConfig("configs/global/paper.yml", "paper.yml")
-    }
 
-    loadFromConfig("configs/worlds/starcade_hub_v2", "world", false)
+    loadFromConfig("configs/global/bukkit.yml", "bukkit.yml")
+    loadFromConfig("configs/global/entropy.yml", "entropy.yml")
+    loadFromConfig("configs/global/pufferfish.yml", "pufferfish.yml")
+    loadFromConfig("configs/global/spigot.yml", "spigot.yml")
+
+    loadFromConfig("configs/global/paper-global.yml", "config/paper-global.yml")
+    loadFromConfig("configs/global/paper-world-defaults.yml", "config/paper-world-defaults.yml")
+
+//    loadFromConfig("configs/worlds/starcade_hub_v2", "world", false)
     clear()
 
-//    loadPlugin("arkgroovy7")
     loadPlugin("starlight8")
     loadPlugin("psql-lib")
     loadPlugin("floodgate-spigot")
-    loadPlugin("ChatControl")
     loadPlugin("EssentialsX-2.19.5-dev+30-b135fcf")
     loadPlugin("PlugMan")
     loadPlugin("ProtocolLib")
@@ -38,26 +39,22 @@ execute {
     loadPlugin("ViaRewind")
     loadPlugin("ViaVersion")
     loadPlugin("LiteBans")
-    loadPlugin("HolographicDisplays")
 
     loadPlugin("NoteBlockAPI")
     loadPlugin("PlaceholderAPI")
 
-    loadPlugin("AnimatedScoreboard")
     loadPlugin("VanishNoPacket")
 
-//    loadConfig("Starlight", "Starlight")
     loadConfig("starlight", "starlight")
     loadFromConfig("configs/proxy/floodgate.yml", "plugins/floodgate/config.yml")
     loadFromConfig("configs/global/floodgate/key.pem", "plugins/floodgate/key.pem")
+
     loadConfig("global/Essentials", "Essentials")
-    loadConfig("global/ChatControlDefaults", "ChatControl")
     loadConfig("global/ViaRewind", "ViaRewind")
     loadConfig("global/ViaVersion", "ViaVersion")
     loadConfig("global/ViaBackwards", "ViaBackwards")
-    loadConfig("global/ChatControl", "ChatControl")
 
     loadConfig("LiteBans${Temple.templeEnv == "dev" ? "-dev" : ""}", "LiteBans")
-    loadConfig("AnimatedScoreboard", "AnimatedScoreboard")
-    loadConfig("HolographicDisplays", "HolographicDisplays")
+//    loadConfig("AnimatedScoreboard", "AnimatedScoreboard")
+//    loadConfig("HolographicDisplays", "HolographicDisplays")
 }
