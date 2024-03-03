f38336554dfdd8894f64561043b0fac4629a8d04
adanilkov <adanilkovas@gmail.com>
2024-03-03 16:06:49 -0500

    homes

--- factions/scripts/factions/content/essentials/Essentials.groovy
+++ factions/scripts/factions/content/essentials/Essentials.groovy
@@ -10,11 +10,15 @@ class Essentials {
             Starlight.unload("~/cmd/EssentialsCmd.groovy")
 
             Starlight.unload("~/warp/Warps.groovy")
+            Starlight.unload("~/homes/Homes.groovy")
             Starlight.unload("~/tp/TeleportHandler.groovy")
+
+            Starlight.unload()
         }
 
         Starlight.watch("~/tp/TeleportHandler.groovy")
         Starlight.watch("~/warp/Warps.groovy")
+        Starlight.watch("~/homes/Homes.groovy")
 
         // cmds
         Starlight.watch("~/cmd/EssentialsCmd.groovy")
