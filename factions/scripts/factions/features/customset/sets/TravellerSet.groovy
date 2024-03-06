fa248e94455439051faf6ab3177d115795e68d63
ily2bb <58750338+ily2bb@users.noreply.github.com>
2024-03-06 01:46:35 -0500

    re-setup modules / starcade

--- factions/scripts/factions/features/customset/sets/TravellerSet.groovy
+++ factions/scripts/factions/features/customset/sets/TravellerSet.groovy
@@ -188,7 +188,7 @@ class TravellerSet extends CustomSet {
                 return false
             }
 
-            if (blocks.isEmpty() || tick >= 350) {
+            if (blocks.isEmpty() || System.currentTimeMillis() - start > 6_000L) {
                 stop()
             } else {
                 tick++
@@ -196,7 +196,6 @@ class TravellerSet extends CustomSet {
         }
 
         def stop() {
-            println("stopping - [${System.currentTimeMillis() - start}ms]")
             active = false
             blocks.each { it.untrack() }
         }
