bddafccd04f65b2f5e49dceece8a9c7dff209cc4
adanilkov <adanilkovas@gmail.com>
2024-03-07 08:46:11 -0500

    dc invites

--- discord/scripts/features/invites/InviteHandler.groovy
+++ discord/scripts/features/invites/InviteHandler.groovy
@@ -247,14 +247,13 @@ class InviteHandler extends ListenerAdapter {
 
 
     static void createLeaderboard(GenericGuildEvent event) {
-        println("250")
+
         def sortedUsers
         Mongo.getGlobal().sync { mongo ->
             sortedUsers = mongo.getCollection(Globals.INVITE_COLLECTION).find().sort(Sorts.descending("realInvites"))
         }
 
         if (sortedUsers == null) println("ERROR could not retrieve sortedUsers")
-        println("257")
 
         String leaderboard = "\n"
         ArrayList<String> places = ["🥇", "🥈", "🥉", "🎖️"]
@@ -264,11 +263,10 @@ class InviteHandler extends ListenerAdapter {
             if (counter > usersOnLeaderBoard) break
             String userId = doc.get("userId")
             String place = counter < 3 ? places[counter] : places[3]
-            Integer invites =  doc.getOrDefault("realInvites", 0) as Integer
+            Integer invites = doc.getOrDefault("realInvites", 0) as Integer
             leaderboard += place + " " + event.getJDA().getUserById(userId).name + " | " + invites + " ${invites == 1 ? "invite" : "invites"} \n"
             counter++
         }
-        println("271")
 
         EmbedBuilder inviteEmbed = new EmbedBuilder()
         inviteEmbed.setTitle("📩 **Starcade Invites**")
@@ -276,13 +274,12 @@ class InviteHandler extends ListenerAdapter {
         inviteEmbed.setDescription(
                 "**Welcome to Starcade Invites!** \r\n Bellow are the invite rankings, reach the top for rewards!\n" + leaderboard
         )
-        println("279")
 
-        TextChannel ticketChannel = event.getJDA().getTextChannelById(Globals.INVITE_CHANNEL_ID)
-        ticketChannel.getHistoryFromBeginning(1).queue {
+        TextChannel inviteChannel = event.getJDA().getTextChannelById(Globals.INVITE_CHANNEL_ID)
+
+        inviteChannel.getHistoryFromBeginning(5).queue {
             List<Message> messages = it.getRetrievedHistory()
             if (messages.size() < 3) {
-                println("285")
 
                 for (Message msg : messages) {
                     println(msg.getId())
@@ -290,8 +287,6 @@ class InviteHandler extends ListenerAdapter {
                         if (msg.isPinned()) continue
                         println(messageId + " - messageId")
                         msg.editMessageEmbeds(inviteEmbed.build()).queue {
-                            println(it.getIdLong() + " - idlong")
-                            println(it.getId() + " - id string")
                             messageId = it.getIdLong()
                         }
                     } catch (Exception ignored) {
@@ -299,18 +294,16 @@ class InviteHandler extends ListenerAdapter {
                     }
                 }
             } else if (messages.size() == 0) {
-                println("302")
 
-                ticketChannel.sendMessageEmbeds(inviteEmbed.build()).queue {
+                inviteChannel.sendMessageEmbeds(inviteEmbed.build()).queue {
                     messageId = it.getIdLong()
                 }
             } else {
-                println("308")
 
                 messages.each {
                     if (!it.isPinned()) it.delete().queue()
                 }
-                ticketChannel.sendMessageEmbeds(inviteEmbed.build()).queue {
+                inviteChannel.sendMessageEmbeds(inviteEmbed.build()).queue {
                     messageId = it.getIdLong()
                 }
             }
