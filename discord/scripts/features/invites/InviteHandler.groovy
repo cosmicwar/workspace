50ec227aec10a7c913afab9bdf06cd70682825d6
adanilkov <adanilkovas@gmail.com>
2024-03-07 08:09:56 -0500

    dc invites

--- discord/scripts/features/invites/InviteHandler.groovy
+++ discord/scripts/features/invites/InviteHandler.groovy
@@ -110,8 +110,9 @@ class InviteHandler extends ListenerAdapter {
 
     @Override
     void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        println("test4")
+        println(event.getName())
         if (event.getName() == "invites") {
-
             getUserInvites(event.user.id) { doc ->
                 if (doc == null) doc = new Document()
 
@@ -119,6 +120,7 @@ class InviteHandler extends ListenerAdapter {
                 event.replyEmbeds(new EmbedBuilder().setTitle("${event.user.name} - ${real} invites.").build()).setEphemeral(true).queue()
             }
         } else if (event.getName() == "inviteleaderboard") {
+            println("test")
             def sortedUsers
             Mongo.getGlobal().sync { mongo ->
                 sortedUsers = mongo.getCollection(Globals.INVITE_COLLECTION).find().sort(Sorts.descending("realInvites"))
@@ -137,6 +139,7 @@ class InviteHandler extends ListenerAdapter {
                 leaderboard += place + " " + event.getJDA().getUserById(userId).name + " | " + doc.getOrDefault("realInvites", 0) + " invites \n"
                 counter++
             }
+            println("test2")
 
             EmbedBuilder inviteEmbed = new EmbedBuilder()
             inviteEmbed.setTitle("📩 **Starcade Invites**")
@@ -144,6 +147,7 @@ class InviteHandler extends ListenerAdapter {
             inviteEmbed.setDescription(
                     "**Welcome to Starcade Invites!** \r\n Bellow are the invite rankings, reach the top for rewards!\n" + leaderboard
             )
+            println("test3")
 
             event.reply("test").setEphemeral(true)
         }
