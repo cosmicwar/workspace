4dd1f1926023a5ba3f391b03ee758cbc02e33296
ily2bb <58750338+ily2bb@users.noreply.github.com>
2024-03-06 19:58:15 -0500

    re-setup modules / starcade

--- factions/scripts/factions/patch/swappatch.groovy
+++ factions/scripts/factions/patch/swappatch.groovy
@@ -15,41 +15,41 @@ import org.starcade.starlight.helper.event.filter.EventFilters
 import org.starcade.starlight.helper.protocol.Protocol
 import org.starcade.starlight.helper.utils.Players
 
-Protocol.subscribe(PacketType.Play.Client.WINDOW_CLICK).handler({
-    if (it.playerTemporary) return
-
-    Player player = it.player
-    PacketContainer packetContainer = it.getPacket()
-    if (packetContainer.getIntegers().read(2) == 40) {
-        it.setCancelled(true)
-        Players.msg(player, "§c§lItem swapping currently disabled!")
-        Schedulers.sync().run {
-            player.updateInventory()
-        }
-    }
-})
-
-Events.subscribe(PlayerSwapHandItemsEvent.class).handler { event ->
-    event.setCancelled(true)
-    event.getPlayer().sendMessage("§c§lItem swapping currently disabled.")
-}
-
-Events.subscribe(InventoryClickEvent.class).filter(EventFilters.ignoreCancelled()).handler { event ->
-    // handling shield swapping to offhand by shift clicking it.
-    ItemStack currentItem = event.getCurrentItem()
-    if ((event.click == ClickType.SHIFT_LEFT || event.click == ClickType.SHIFT_RIGHT) && currentItem != null && currentItem.getType() == Material.SHIELD) {
-        event.setCancelled(true)
-    }
-
-    if (event.click != ClickType.SWAP_OFFHAND) return
-    event.setCancelled(true)
-    event.getWhoClicked().sendMessage("§c§lItem swapping currently disabled.")
-}
-
-Events.subscribe(CraftItemEvent.class).filter(EventFilters.ignoreCancelled()).handler({ event ->
-    if (event.click != ClickType.SWAP_OFFHAND) return
-
-    event.setCancelled(true)
-    Player player = event.getWhoClicked() as Player
-    Players.msg(player, "§c§lItem swapping currently disabled.")
-})
+//Protocol.subscribe(PacketType.Play.Client.WINDOW_CLICK).handler({
+//    if (it.playerTemporary) return
+//
+//    Player player = it.player
+//    PacketContainer packetContainer = it.getPacket()
+//    if (packetContainer.getIntegers().read(2) == 40) {
+//        it.setCancelled(true)
+//        Players.msg(player, "§c§lItem swapping currently disabled!")
+//        Schedulers.sync().run {
+//            player.updateInventory()
+//        }
+//    }
+//})
+//
+//Events.subscribe(PlayerSwapHandItemsEvent.class).handler { event ->
+//    event.setCancelled(true)
+//    event.getPlayer().sendMessage("§c§lItem swapping currently disabled.")
+//}
+//
+//Events.subscribe(InventoryClickEvent.class).filter(EventFilters.ignoreCancelled()).handler { event ->
+//    // handling shield swapping to offhand by shift clicking it.
+//    ItemStack currentItem = event.getCurrentItem()
+//    if ((event.click == ClickType.SHIFT_LEFT || event.click == ClickType.SHIFT_RIGHT) && currentItem != null && currentItem.getType() == Material.SHIELD) {
+//        event.setCancelled(true)
+//    }
+//
+//    if (event.click != ClickType.SWAP_OFFHAND) return
+//    event.setCancelled(true)
+//    event.getWhoClicked().sendMessage("§c§lItem swapping currently disabled.")
+//}
+//
+//Events.subscribe(CraftItemEvent.class).filter(EventFilters.ignoreCancelled()).handler({ event ->
+//    if (event.click != ClickType.SWAP_OFFHAND) return
+//
+//    event.setCancelled(true)
+//    Player player = event.getWhoClicked() as Player
+//    Players.msg(player, "§c§lItem swapping currently disabled.")
+//})
