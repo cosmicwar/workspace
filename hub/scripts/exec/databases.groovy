a0f0214763c8b8c803898d5cbfd934ac21020ecf
ily2bb <58750338+ily2bb@users.noreply.github.com>
2024-05-13 11:29:01 -0400

    re-setup modules / starcade

--- hub/scripts/exec/databases.groovy
+++ hub/scripts/exec/databases.groovy
@@ -26,8 +26,8 @@ Exports.ptr("mysql_creds", [
         ]
 ])
 
-Redis.register("local", new HelperRedis(RedisCredentials.of("172.93.103.228", 6391, "Fd12Ez7TS5bVeL2xDsAiWyH2JPdZUSGO7KVYi9hQJVoLe03j5HQSZhSrG3/XczxMOCmgHFKUdkxvsi4m")))
-Redis.register("global", new HelperRedis(RedisCredentials.of("172.93.103.228", 6391, "Fd12Ez7TS5bVeL2xDsAiWyH2JPdZUSGO7KVYi9hQJVoLe03j5HQSZhSrG3/XczxMOCmgHFKUdkxvsi4m")))
+Redis.register("local", new HelperRedis(RedisCredentials.of("172.93.103.228", 6391, "Fd12Ez7TS5bVeL2xDsAiWyH2JPdZUSGO7KVYi9hQJVoLe03j5HQSZhSrG3")))
+Redis.register("global", new HelperRedis(RedisCredentials.of("172.93.103.228", 6391, "Fd12Ez7TS5bVeL2xDsAiWyH2JPdZUSGO7KVYi9hQJVoLe03j5HQSZhSrG3")))
 
 Mongo.register("local", new HelperMongo(MongoDatabaseCredentials.of("172.93.103.228", 27335, "admin", "starcade", "gAWbxWHHenIFWfBcpzLuLB33KhdyVfDO")), "starcade")
 Mongo.register("global", new HelperMongo(MongoDatabaseCredentials.of("172.93.103.228", 27335, "admin", "starcade", "gAWbxWHHenIFWfBcpzLuLB33KhdyVfDO")), "starcade")
