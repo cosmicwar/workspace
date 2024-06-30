bee271d45a40d47fe57cd1bd482a835e5f6d8742
adanilkov <adanilkovas@gmail.com>
2024-06-29 21:23:46 -0400

    setup scripts

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-java -jar ../../shared/jars/factionsbootstrap.jar proxy
+java -jar ../shared/jars/factionsbootstrap.jar proxy
 echo "If you want to completely stop the server process now, press Ctrl+C before
 the time is up!"
 echo "Rebooting in:"
