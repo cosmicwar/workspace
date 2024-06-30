4d4812bb9587ff98af80b5618dcad4c3fe8e29b3
adanilkov <adanilkovas@gmail.com>
2024-06-30 16:24:46 -0400

    setup

--- proxy/scripts/exec/databases.groovy
+++ proxy/scripts/exec/databases.groovy
@@ -7,8 +7,8 @@ import org.starcade.starlight.helper.redis.plugin.HelperRedis
 import scripts.shared3.Mongo
 import scripts.shared3.Redis
 
-Redis.register("local", new HelperRedis(RedisCredentials.of("104.243.38.125", 6391, "Hpkfp2jkn4+yKLi501GU5TY4UGEcVV7mobOKFAz32oSGKSos6FymAPF29Cn8+roRZc9bCMJCuFQLKDu4")))
-Redis.register("global", new HelperRedis(RedisCredentials.of("104.243.38.125", 6391, "Hpkfp2jkn4+yKLi501GU5TY4UGEcVV7mobOKFAz32oSGKSos6FymAPF29Cn8+roRZc9bCMJCuFQLKDu4")))
+Redis.register("local", new HelperRedis(RedisCredentials.of("127.0.0.1", 6391, "Hpkfp2jkn4+yKLi501GU5TY4UGEcVV7mobOKFAz32oSGKSos6FymAPF29Cn8+roRZc9bCMJCuFQLKDu4")))
+Redis.register("global", new HelperRedis(RedisCredentials.of("127.0.0.1", 6391, "Hpkfp2jkn4+yKLi501GU5TY4UGEcVV7mobOKFAz32oSGKSos6FymAPF29Cn8+roRZc9bCMJCuFQLKDu4")))
 
 //left same
 Mongo.register("local", new HelperMongo(MongoDatabaseCredentials.of("104.243.38.125", 27335, "admin", "starcade", "gAWbxWHHenIFWfBcpzLuLB33KhdyVfDO")), "starcade")
