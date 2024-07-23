5ac1f8169db1aad12153639f018f69c1a1fb84a5
adanilkov <adanilkovas@gmail.com>
2024-07-22 21:50:27 -0400

    setup

--- factions/scripts/exec/nova1/_execute.groovy
+++ factions/scripts/exec/nova1/_execute.groovy
@@ -8,8 +8,8 @@ import static scripts.execute.ArkRepo.*
 setVersion("v1_20_R1")
 setWorkspaceName("factions")
 
-//TODO: change Xmx back to 64G, set to 25G for local use
-addArgs("-Xms25G", "-Xmx25G", "-DPaper.ignoreWorldDataVersion=true", "--add-modules=jdk.incubator.vector")
+
+addArgs("-Xms25G", /*"-Xmx64G", */"-DPaper.ignoreWorldDataVersion=true", "--add-modules=jdk.incubator.vector")
 
 addProgramArgs("--nogui")
 
